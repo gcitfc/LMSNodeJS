750ea98aed1a47ef85d6db447b7ed26df077d9c5
gcitfc <chen_fang@gcitsolutions.com>
2019-12-19 12:23:09 -0500

    Change Author queries to React convention

--- lms/controllers/authorController.js
+++ lms/controllers/authorController.js
@@ -2,47 +2,55 @@ var routes = require('express').Router();
 var db = require('../dao/db');
 var authorDao = require('../dao/authorDao');
 
-routes.get('/author',function(req,res){
-    authorDao.getAllAuthors(function(error, result){
-      if(error) throw error;
-      res.setHeader('Content-Type', 'application/json');
-      res.send(result);
-    });
+routes.get('/author', function(req,res){
+  authorDao.getAllAuthors()
+  .then(function(result){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(result);
+  })
+  .catch(function(err){
+    throw err;
+  });
 });
 
-routes.post('/author', function(req, res){
+routes.post('/author', function(req, res) {
   var author = req.body;
-  authorDao.addAuthor(author, function(err, result){
-    if(err){
-      res.status(400);
-      res.send('Add Author Failed!');
-    }
+  authorDao.addAuthor(author)
+  .then(function(result) {
     res.status(201);
     res.send('Add Author Successful!');
+  })
+  .catch(function(err){
+    res.status(400);
+    res.send('Add Author Failed!');
+    throw err;
   });
-
 });
 
-routes.put('/author', function(req, res){
+routes.put('/author', function(req, res) {
   var author = req.body;
-  authorDao.updateAuthor(author, function(err, result){
-    if(err){
-      res.status(400);
-      res.send('Update Author Failed!');
-    }
+  authorDao.updateAuthor(author)
+  .then(function(result) {
     res.status(201);
     res.send('Update Author Successful!');
+  })
+  .catch(function(err){
+    res.status(400);
+    res.send('Update Author Failed!');
+    throw err;
   });
-
 });
 
+
 routes.delete('/author/:id', function(req, res){
-  authorDao.removeAuthor(req.params.id, function(err, result){
-    if(err){
-      res.status(400);
-      res.send('Delete Author Failed!');
-    }
+  authorDao.removeAuthor(req.params.id)
+  .then(function(result) {
     res.send('Delete Author Successful!');
+  })
+  .catch(function(err){
+    res.status(400);
+    res.send('Delete Author Failed!');
+    throw err;
   });
 });
 
--- lms/dao/authorDao.js
+++ lms/dao/authorDao.js
@@ -1,58 +1,33 @@
 var db = require('./db');
 
-exports.getAllAuthors = function(cb){
-    db.query('select * from lmsdb.author', function(err, result) {
-        cb(err, result);
+exports.getAllAuthors = function(){
+  return new Promise(function(resolve, reject){
+      db.query('select * from lmsdb.author', function(err, result){            
+          return err ? reject(err) : resolve(result);
       });
+  });
 };
 
 exports.addAuthor = function(author, cb){
-  db.beginTransaction(function(err){
-      if(err) cb(err, null);
-  
-      db.query('insert into lmsdb.author (author_name) values (?)', [author.author_name], function(err, res){
-        if(err){
-          db.rollback(function(err, res){
-            cb(err, res);
-          });
-        } 
-        db.commit(function(err, res){
-          cb(err, res);
-        });
-      });
+  return new Promise(function(resolve, reject){
+    db.query('insert into lmsdb.author (author_name) values (?)', [author.author_name], function(err, result){            
+        return err ? reject(err) : resolve(result);
     });
+  });
 };
 
 exports.updateAuthor = function(author, cb){
-db.beginTransaction(function(err){
-    if(err) cb(err, null);
-
-    db.query('update lmsdb.author set author_name = ? where author_id = ?', [author.author_name, author.author_id], function(err, res){
-      if(err){
-        db.rollback(function(err, res){
-          cb(err, res);
-        });
-      } 
-      db.commit(function(err, res){
-        cb(err, res);
-      });
+  return new Promise(function(resolve, reject){
+    db.query('update lmsdb.author set author_name = ? where author_id = ?', [author.author_name, author.author_id], function(err, result){            
+        return err ? reject(err) : resolve(result);
     });
   });
 };
 
 exports.removeAuthor = function(authorId, cb){
-  db.beginTransaction(function(err){
-      if(err) cb(err, null);
-  
-      db.query('delete from lmsdb.author where author_id = ?', [authorId], function(err, res){
-        if(err){
-          db.rollback(function(err, res){
-            cb(err, res);
-          });
-        } 
-        db.commit(function(err, res){
-          cb(err, res);
-        });
-      });
+  return new Promise(function(resolve, reject){
+    db.query('delete from lmsdb.author where author_id = ?', [authorId], function(err, result){            
+        return err ? reject(err) : resolve(result);
     });
-}
+  });
+};
